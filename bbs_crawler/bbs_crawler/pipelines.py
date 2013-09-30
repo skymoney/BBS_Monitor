88a49a497cf090653410f2ff6d1d5fa02b38a697
cheng <CChain0615@gmail.com>
2013-09-30 15:56:47 +0800

    add pipeline method

--- bbs_crawler/bbs_crawler/pipelines.py
+++ bbs_crawler/bbs_crawler/pipelines.py
@@ -3,6 +3,16 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+from bbs_crawler.items import Lily
+
+import simplejson as json
+import redis
+
 class BbsCrawlerPipeline(object):
-    def process_item(self, item, spider):
-        return item
+	def __init__(self):
+		self.lily_file=open('lily.json','wb')
+
+	def process_item(self, item, spider):
+		if isinstance(item,Lily):
+			self.lily_file.write(json.dumps(dict(item))+'\n')
+		return item
